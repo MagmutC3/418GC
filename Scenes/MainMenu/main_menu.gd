3409e08eb210bdafee12692dc0c7fee8c2420ff7
strunk23 <miloszwiktor58@gmail.com>
2025-11-21 23:25:03 +0100

    Main Menu UI Functionality

--- Scenes/MainMenu/main_menu.gd
+++ Scenes/MainMenu/main_menu.gd
@@ -1,19 +1,9 @@
 extends Control
 
-## In this file you can find an embryo of localization logic
-#func _ready():
-	#TranslationServer.set_locale("en")
 
 func _input(event):
 	if visible and Input.is_action_just_pressed("ui_accept"):
 		_on_start_button_pressed()
-
-	#if event is InputEventKey and event.pressed:
-		#if event.physical_keycode == KEY_L:
-			#if TranslationServer.get_locale() == "en":
-				#TranslationServer.set_locale("ru")
-			#else:
-				#TranslationServer.set_locale("en")
 
 func _on_start_button_pressed():
 	AudioManager.SFX.play_sfx("click")
@@ -21,3 +11,13 @@ func _on_start_button_pressed():
 	AudioManager.Music.play_main_theme()
 	
 	TransitionManager.change_scene("res://Scenes/MainGame/MainGame.tscn")
+
+
+func _on_reset_save_button_pressed() -> void:
+	AudioManager.SFX.play_sfx("click")
+	TransitionManager.change_scene("res://Scenes/ResetSave/ResetSave.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	AudioManager.SFX.play_sfx("click")
+	get_tree().quit()
